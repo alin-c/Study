9d0384b80c66312b6dc1060ddddc5227ff7ec0fe
Alin <alin.clincea@outlook.com>
2020-08-08 11:27:42 +0300

    * working example

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using static System.Console; // since C# 6; allows using a member directly
 
 namespace Delegates {
+	// 1. declare a delegate type
+	delegate void PrintDelegate(int value);
+
 	class Program {
-		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+		// 2. declare methods with matching signatures
+		static void PrintLow(int value) {
+			WriteLine($"{value} - Low");
+		}
+
+		static void PrintHigh(int value) {
+			WriteLine($"{value} - High");
+		}
+
+		static void Main() {
+			// 3. declare a delegate variable
+			PrintDelegate printDelegate;
+
+			Random random = new Random();
+			int randomValue = random.Next(100);
+
+			// 4. instantiate the variable
+			printDelegate = randomValue < 50 ? new PrintDelegate(PrintLow) : new PrintDelegate(PrintHigh);
+
+			// 5. execute (invoke) the delagate
+			printDelegate(randomValue);
 		}
 	}
 }
